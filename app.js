3bc0a0b803b5cf02579a5fb8013a1540d833ec7f
Karlen Avetyan <karlenavetyan@gmail.com>
2022-05-17 15:52:26 +0400

    Push

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const nodeHtmlToImage = require('node-html-to-image');
+const puppeteer = require('puppeteer');
 
 const app = express();
+
 app.use(
 router.get(`/`, async function(req, res) {
-    const image = await nodeHtmlToImage({
-        html: `<html><body><div>Check out what ${req.query?.text} I just did! #cool</div></body></html>`
-    });
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.setViewport({ width: 1200, height: 650 })
+    await page.goto(`https://karl.am/quoteBot/view.php?type=${req.query?.text}&text=${req.query?.text}&name=${req.query?.name}`)
+    const img = await page.screenshot()
+    await browser.close()
+
     res.writeHead(200, { 'Content-Type': 'image/png' });
-    res.end(image, 'binary');
+    res.end(img, 'binary');
 }));
 
 app.listen(8080, () => console.log('app running on 8080 port'))
