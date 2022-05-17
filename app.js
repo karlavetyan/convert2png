c58daccf10ec274aa86a02005b59748fdb8a6b7d
Karlen Avetyan <karlenavetyan@gmail.com>
2022-05-17 11:24:00 +0400

    make it better

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require('express');
 const router = express.Router();
 const nodeHtmlToImage = require('node-html-to-image');
 
-router.get(`/api/tweet/render`, async function(req, res) {
+const app = express();
+app.use(
+router.get(`/`, async function(req, res) {
     const image = await nodeHtmlToImage({
-        html: '<html><body><div>Check out what I just did! #cool</div></body></html>'
+        html: `<html><body><div>Check out what ${req.query?.text} I just did! #cool</div></body></html>`
     });
     res.writeHead(200, { 'Content-Type': 'image/png' });
     res.end(image, 'binary');
-});
+}));
+
+app.listen(8000, () => console.log('app running on 8000 port'))
+
