ebb9fc686a93e02df1f453bf9ba1b999a06f1e26
Karlen Avetyan <karlenavetyan@gmail.com>
2022-05-18 17:28:17 +0400

    Push

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const nodeHtmlToImage = require('node-html-to-image');
+const { chromium } = require("playwright");
 
 const app = express();
+
 app.use(
-    router.get(`/`, async function(req, res) {
-        const image = await nodeHtmlToImage({
-            html: `<html><body><div>Check out what ${req.query?.text} I just did! #cool</div></body></html>`
-        });
-        res.writeHead(200, { 'Content-Type': 'image/png' });
-        res.end(image, 'binary');
-    }));
+router.get(`/`, async function(req, res) {
+    let browser = await chromium.launch();
+
+    let page = await browser.newPage();
+    await page.setViewportSize({ width: 1200, height: 650 });
+    await page.goto(`https://karl.am/quoteBot/view.php?type${req.query?.text}&text=${req.query?.text}&name=${req.query?.name}`);
+    const buffer = await page.screenshot();
+    await browser.close();
+
+    res.writeHead(200, { 'Content-Type': 'image/png' });
+    res.end(buffer, 'binary');
+}));
 
 app.listen(80, () => console.log('app running on 80 port'))
 
