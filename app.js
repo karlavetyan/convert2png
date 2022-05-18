9540bc2f6896f50d1d505b1a282653415f750d24
Karlen Avetyan <karlenavetyan@gmail.com>
2022-05-18 17:05:26 +0400

    Push

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const screen = require("node-server-screenshot");
+const nodeHtmlToImage = require('node-html-to-image');
 
 const app = express();
-
 app.use(
-router.get(`/`, async function(req, res) {
-    const img = await screen.fromURL(`https://karl.am/quoteBot/view.php?type=${req.query?.text}&text=${req.query?.text}&name=${req.query?.name}`);
-
-    res.writeHead(200, { 'Content-Type': 'image/png' });
-    res.end(img, 'binary');
-}));
+    router.get(`/`, async function(req, res) {
+        const image = await nodeHtmlToImage({
+            html: `<html><body><div>Check out what ${req.query?.text} I just did! #cool</div></body></html>`
+        });
+        res.writeHead(200, { 'Content-Type': 'image/png' });
+        res.end(image, 'binary');
+    }));
 
 app.listen(8080, () => console.log('app running on 8080 port'))
 
