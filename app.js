d345276c41d38be5f4bcf0a60a226730e69a07ba
Karlen Avetyan <karlenavetyan@gmail.com>
2022-05-20 11:28:26 +0400

    Push

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-const http = require('http');
+const express = require('express');
+const router = express.Router();
 const { chromium } = require("playwright");
 
+const app = express();
 
-http.createServer(async function (req, res) {
+app.use(
+router.get(`/`, async function(req, res) {
     let browser = await chromium.launch();
 
     let page = await browser.newPage();
     await page.setViewportSize({ width: 1200, height: 650 });
-    await page.goto(`http://159.223.232.30/quoteBot/view.php?type${req.query?.text}&text=${req.query?.text}&name=${req.query?.name}`);
+    await page.goto(`https://karl.am/quoteBot/view.php?type${req.query?.text}&text=${req.query?.text}&name=${req.query?.name}`);
     const buffer = await page.screenshot();
     await browser.close();
 
     res.writeHead(200, { 'Content-Type': 'image/png' });
     res.end(buffer, 'binary');
-}).listen(80, '127.0.0.1');
+}));
+
+app.listen(26, () => console.log('app running on 26 port'))
+
