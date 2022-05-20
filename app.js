cfb3f9db1bdd9f6333c4486ebdfab63b7ba3d0a8
Karlen Avetyan <karlenavetyan@gmail.com>
2022-05-20 10:52:36 +0400

    Push

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
-const express = require('express');
-const router = express.Router();
+const http = require('http');
 const { chromium } = require("playwright");
 
-const app = express();
 
-app.use(
-router.get(`/`, async function(req, res) {
+http.createServer(async function (req, res) {
     let browser = await chromium.launch();
 
     let page = await browser.newPage();
     await page.setViewportSize({ width: 1200, height: 650 });
-    await page.goto(`http://134.209.204.71/quoteBot/view.php?type${req.query?.text}&text=${req.query?.text}&name=${req.query?.name}`);
+    await page.goto(`http://159.223.232.30/quoteBot/view.php?type${req.query?.text}&text=${req.query?.text}&name=${req.query?.name}`);
     const buffer = await page.screenshot();
     await browser.close();
 
     res.writeHead(200, { 'Content-Type': 'image/png' });
     res.end(buffer, 'binary');
-}));
-
-app.listen(2626, () => console.log('app running'))
-
+}).listen(80, '127.0.0.1');
